55d80a324fa2d2a2d2f8420c16ee5f881dbbe6b2
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-05-30 16:17:23 +0100

    Revisited 33

--- sols/33.py
+++ sols/33.py
@@ -24,6 +24,22 @@ class Solution:
 
         return low if nums[low] == target else -1
 
+    # Modified Binary Search (Revisited), O(log n) time, O(1) space
+    # def search(self, nums: List[int], target: int) -> int:
+    #     if not nums:
+    #         return -1
+    #     first, low, high = nums[0], 0, len(nums)-1
+    #     while low <= high:
+    #         mid = (low + high) // 2
+    #         val = nums[mid]
+    #         if val == target:
+    #             return mid
+    #         elif first<=val<target or val<target<first or target<first<=val:
+    #             low = mid+1
+    #         else:
+    #             high = mid-1
+    #     return -1
+
     # # Modified Binary Search (Top Voted), O(log n) time, O(1) space
     # def search(self, nums: List[int], target: int) -> int:
 
