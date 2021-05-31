6027b57bc7bf9bcd82dbc4f856bbbb7fa8f69f9f
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-05-31 17:40:35 +0100

    Revisited 80

--- sols/80.py
+++ sols/80.py
@@ -17,6 +17,25 @@ class Solution:
     #             i += 1
     #     return i
 
+    # # Two pointers (Revisited), O(n) time, O(1) space
+    # def removeDuplicates(self, nums: List[int]) -> int:
+    #     res = 0
+    #     cur = None
+    #     cur_cnt = 0
+    #     cur_ind = 0
+    #     for i in range(len(nums)):
+    #         if nums[i] == cur:
+    #             if cur_cnt == 2:
+    #                 continue
+    #             cur_cnt += 1
+    #         else:
+    #             cur = nums[i]
+    #             cur_cnt = 1
+    #         nums[cur_ind] = nums[i]
+    #         cur_ind += 1
+    #         res += 1
+    #     return res
+
     # Setting first n elements (Top Voted), O(n) time, O(1) space
     def removeDuplicates(self, nums: List[int]) -> int:
         i = 0
