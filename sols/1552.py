9772f799be4424d7fe5c11309148e027dd01f0b5
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-06-01 23:00:14 +0100

    Revisited 1552

--- sols/1552.py
+++ sols/1552.py
@@ -30,6 +30,33 @@ class Solution:
 
     #     return low if count(low) >= m else -1
 
+    # # Binary Search on Count (Revisited), O(n (log m + log n)) time, O(1) space
+    # def maxDistance(self, position: List[int], m: int) -> int:
+    #     position.sort()
+
+    #     def count(force):
+    #         cnt = 0
+    #         cur = position[0]
+    #         for n in position:
+    #             if n >= cur:
+    #                 cur = n + force
+    #                 cnt += 1
+    #         return cnt
+
+    #     # Binary search for the minimum force
+    #     low, high = 0, position[-1] - position[0]
+    #     while low < high:
+    #         mid = (low + high + 1) // 2
+    #         print(low, high, mid)
+    #         print(count(mid))
+    #         if count(mid) >= m:
+    #             # More balls than needed, can try larger force
+    #             low = mid
+    #         else:
+    #             # Not enough balls fitted, reduce force
+    #             high = mid - 1
+    #     return low
+
     # Binary Search on Count (Top Voted), O(n (log m + log n)) time, O(1) space
     def maxDistance(self, position: List[int], m: int) -> int:
         n = len(position)
