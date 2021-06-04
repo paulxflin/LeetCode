dc1f810d10868e29d8699092021882185d792bfd
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-06-04 22:07:12 +0100

    787. Cheapest Flights Within K Stops

--- README.md
+++ README.md
@@ -64,6 +64,7 @@ Categories:
 | 147  | Insertion Sort List                    | Dummy + Predecessor + Disorder, T: O(n^2), S: O(1) |
 | 464  | Can I Win                              | Top Down DP choices key, T: O(n \* 2^k), S: O(k!)  |
 | 676  | Implement Magic Dictionary             | Diff = 1 for List/dict, Candidates, T&S: O(n \* s) |
+| 787  | Cheapest Flights Within K Stops        | BFS, T&S Unknown                                   |
 | 955  | Delete Columns to Make Sorted II       | Track unsorted strs using set, T: O(n\*m), S: O(n) |
 | 1552 | Magnetic Force Between Two Balls       | Binary Search, T: O(n \* (log n + log m)), S: O(1) |
 | 1727 | Largest Submatrix With Rearrangements  | Sum column stacks, T: O(n \* m log m), S: O(1)     |
---
+++ sols/787.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    # BFS (Top Voted), Unknown Time and Space
+    def findCheapestPrice(self, n, flights, src, dst, K):
+        if src == dst:
+            return 0
+        d, seen = collections.defaultdict(
+            list), collections.defaultdict(lambda: float('inf'))
+        for u, v, p in flights:
+            d[u] += [(v, p)]
+
+        q = [(src, -1, 0)]
+
+        while q:
+            pos, k, cost = q.pop(0)
+            if pos == dst or k == K:
+                continue
+            for nei, p in d[pos]:
+                if cost + p >= seen[nei]:
+                    continue
+                else:
+                    seen[nei] = cost+p
+                    q += [(nei, k+1, cost+p)]
+
+        return seen[dst] if seen[dst] < float('inf') else -1
