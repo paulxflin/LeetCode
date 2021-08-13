5b8eff2b98d2df70777d6e1e1ead325e57baba76
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-08-13 18:06:01 +0100

    Revisit 1109 (attempt again, update idea)

--- sols/1109.py
+++ sols/1109.py
@@ -8,3 +8,13 @@ class Solution:
         for i in range(1, n):
             res[i] += res[i - 1]
         return res[:-1]
+
+    # Sweep Lines (Revisited), O(b + n) time, O(n) space
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        ans = [0]*(n+1)  # Add 1 for the flight n case
+        for first, last, seats in bookings:
+            ans[first-1] += seats
+            ans[last] -= seats
+        for i in range(1, n+1):
+            ans[i] += ans[i-1]
+        return ans[:-1]
