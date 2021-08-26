2c16f2dac7ec58545a3841c93a9b4086f646e78f
Xiaofeng Paul Lin <paullam11100@hotmail.com>
2021-08-26 23:37:42 +0100

    Shorter and Faster Accepted Sol for 1556

--- sols/1556.py
+++ sols/1556.py
@@ -18,6 +18,12 @@ class Solution:
             i += 3
         return '.'.join(l)
 
+    # Conditional + List Comprehension (Accepted), O(n) time and space
+    def thousandSeparator(self, n: int) -> str:
+        s = str(n)
+        r = len(s) % 3
+        return '.'.join(([s[:r]] if r else []) + [s[i:i+3] for i in range(r, len(s), 3)])
+
     # Reverse (Top Voted), O(n) time and space
     def thousandSeparator(self, n: int) -> str:
         s = str(n)
